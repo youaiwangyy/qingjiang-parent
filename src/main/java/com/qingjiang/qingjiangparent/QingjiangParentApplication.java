4ecee8187484f690fc83c26dd78bb112eba4898c
youaiwangyy <qing_jiang_li@163.com>
2018-11-15 02:06:05 +0800

    init project commit

---
+++ src/main/java/com/qingjiang/qingjiangparent/QingjiangParentApplication.java
@@ -0,0 +1,12 @@
+package com.qingjiang.qingjiangparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingjiangParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QingjiangParentApplication.class, args);
+	}
+}
